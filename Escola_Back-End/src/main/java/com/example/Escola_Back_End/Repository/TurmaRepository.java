359576e44b2cc27caeb8375372383f1f9c0ec40d
alexsant22 <alexandre.santana2201@gmail.com>
2025-04-30 14:59:30 -0300

    Repository

--- Escola_Back-End/src/main/java/com/example/Escola_Back_End/Repository/TurmaRepository.java
+++ Escola_Back-End/src/main/java/com/example/Escola_Back_End/Repository/TurmaRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TurmaRepository extends JpaRepository<Turma, Long> {
-    List<Turma> findByNome(String nome);
+    List<Turma> getAllByNome(String nome);
 }
